9f2f7f4493505fa02d5c4853960b252c161e161f
ZizaAli <ziza.aliyyeva@gmail.com>
2025-01-26 15:37:07 +0100

    new repo

---
+++ src/lesson_6.java
@@ -0,0 +1,38 @@
+//public class lesson_6 {
+//    public static void main(String[] args) {
+//        //TAPSIRIQ-2
+//       int a = 15;
+//       byte b = 20;
+//       short c = 10;
+//       long d = 150;
+//       double s = 4.5;
+//       float f = 2.5f;
+//       char v = 'A';
+//       boolean m, x  = true;
+//       boolean n, y = false;
+//       int a = 15;
+//       int b = 20;
+//       boolean cavab = a>b;
+//       System.out.println(cavab);
+////        //cavab=false
+//
+//        //TAPSIRIQ-3
+//        int [] deyerler = {10,20,30,40,50};
+//        System.out.println(deyerler[0]+deyerler[4]);
+//////        cavab =60
+////
+//        //TAPSIRIQ-4
+//        String ad = "Aziza";
+//        String soyad = "Aliyeva";
+//        System.out.println(ad + " " + soyad);
+//        //netice = Aziza Aliyeva
+//
+//        }
+//                }
+//        //tapsiriq 3-u yazanda pront ederken Array.deepToString yazanda error oldu
+//        public class lesson_6 {
+//        public static void main(String[] args) {
+//            int [] deyerler = {10,20,30,40,50};
+//            System.out.println(Arrays.deepTostring(deyerler[0]+deyerler[4]));
+//  }
+// }
