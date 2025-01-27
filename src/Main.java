f4dff6cd0ca3ad6239c85a485bd4933f4dcce357
ZizaAli <ziza.aliyyeva@gmail.com>
2025-01-27 16:11:57 +0100

    task4

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,49 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
-    }
-}
+
+//public class lesson_6 {
+//    public static void main(String[] args) {
+//        //TAPSIRIQ-2
+//       int a = 15;
+//       byte b = 20;
+//       short c = 10;
+//       long d = 150;
+//       double s = 4.5;
+//       float f = 2.5f;
+//       char v = 'A';
+//       boolean m, x  = true;
+//       boolean n, y = false;
+//       int a = 15;
+//       int b = 20;
+//       boolean cavab = a>b;
+//       System.out.println(cavab);
+////        //cavab=false
+//
+//        //TAPSIRIQ-3
+//        int [] deyerler = {10,20,30,40,50};
+//        System.out.println(deyerler[0]+deyerler[4]);
+//////        cavab =60
+////
+//        //TAPSIRIQ-4
+//        String ad = "Aziza";
+//        String soyad = "Aliyeva";
+//        System.out.println(ad + " " + soyad);
+//        //netice = Aziza Aliyeva
+//
+//        }
+//                }
+//        //tapsiriq 3-u yazanda pront ederken Array.deepToString yazanda error oldu
+//        public class lesson_6 {
+//        public static void main(String[] args) {
+//            int [] deyerler = {10,20,30,40,50};
+//            System.out.println(Arrays.deepTostring(deyerler[0]+deyerler[4]));
+//  }
+// }
+
+//Tapsiriq 4
+class Main {
+
+        public static void main(String[] args) {
+            String[] ad_soyad = {"Aziza", "Aliyeva"};
+            System.out.println(ad_soyad[0]+ " "+ ad_soyad[1]);
+  }
+ }
