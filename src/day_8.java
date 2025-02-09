7de7569fddb72407c85d80e19ce86d2c1dc7580b
ZizaAli <ziza.aliyyeva@gmail.com>
2025-02-09 19:22:18 +0100

    test

--- src/day_8.java
+++ src/day_8.java
@@ -62,22 +62,142 @@
 //}
 
 
+//   public class day_8 {
+//       public static void main(String[] args) {
+//           int[] a={5,9,23,78,90,3,64,12,5,9,90,5,3,9};
+//           for(int i = 0; i < a.length; i ++){
+//               for(int j =i+1; j<a.length; j++){
+//                   if(a[i]==a[j]){
+//                       System.out.println(a[i]);
+//                   }
+//               }
+//           }
+//       }
+//
+//
+//       }
 
-   public class day_8 {
-       public static void main(String[] args) {
-           int[] a={5,9,23,78,90,3,64,12,5,9,90,5,3,9};
-           for(int i = 0; i < a.length; i ++){
-               for(int j =i+1; j<a.length; j++){
-                   if(a[i]==a[j]){
-                       System.out.println(a[i]);
-                   }
-               }
-           }
-       }
+//OOP TASKS
+//public class day_8 {
+//    public static void main(String[] args) {
+//        class NewCar {
+//            int say;
+//            String ad;
+//            String reng;            // masinin rengi
+//            boolean buraxilish_teze;    //islenmis ya teze olmasi
+//            boolean buraxilish_evvelki;
+//            int qapi_sayi;
+//            String model;          // modelin adi
+//        }
+//        NewCar car1 = new NewCar();
+//        car1.say = 150;
+//        car1.ad = "Audi Smart";
+//        car1.reng = "qara";
+//        car1.model= "Audi2025";
+//        car1.qapi_sayi = 2;;
+//        System.out.println(car1.say);
+//        System.out.println(car1.reng);
+//        System.out.println(car1.model);
+//        System.out.println(car1.qapi_sayi);
 
+//        NewCar car2 = new NewCar();
+//        car2.say = 50;
+//        car2.ad = "Audi Q7";
+//        car2.reng = "Ag";
+//        car2.model = "Audi2020";
+//        car2.qapi_sayi = 4;
+//        System.out.println(car2.say);
+//        System.out.println(car2.reng);
+//        System.out.println(car2.model);
+//        System.out.println(car2.qapi_sayi);
 
-       }
+//        NewCar car3 = new NewCar();
+//        car3.say = 120;
+//        car3.ad = "Mersedes AMG";
+//        car3.reng = "Gold";
+//        car3.model = "Mersedes 2025";
+//        car3.qapi_sayi = 5;
+//        System.out.println(car3.say);
+//        System.out.println(car4.ad);
+//        System.out.println(car3.reng);
+//        System.out.println(car3.model);
+//        System.out.println(car3.qapi_sayi);
 
+//        NewCar car4 = new NewCar();
+//        car4.say = 45;
+//        car4.ad = "Tesla fly";
+//        car4.reng = "white";
+//        car4.model = "Tesla 2025";
+//        car4.qapi_sayi = 4;
+//        System.out.println(car4.say);
+//        System.out.println(car4.reng);
+//        System.out.println(car4.ad);
+//        System.out.println(car4.model);
+//        System.out.println(car4.qapi_sayi);
 
+//        }
+//}
+
+//public class day_8 {
+//    public static void main(String[] args) {
+//        class NewBuldig {
+//            int mertebe_sayi;
+//            String seher;
+//            int sayi;
+//            String dizayn;
+//            int balkon_sayi;
+//            String reng;
+//        }
+//        NewBuldig bulding1 = new NewBuldig();
+//        bulding1.balkon_sayi =2;
+//        bulding1.dizayn = "Elit";
+//        bulding1.mertebe_sayi = 21;
+//        bulding1.seher = "Baki";
+//        bulding1.reng = "Ag-Qara- shahmat";
+//        System.out.println(bulding1.balkon_sayi);
+//        System.out.println(bulding1.dizayn);
+//        System.out.println(bulding1.sayi)
+//        System.out.println(bulding1.mertebe_sayi);
+//        System.out.println(bulding1.reng);
+//        System.out.println(bulding1.reng);
+//    }
+//}
+
+public class day_8 {
+    public static void main(String[] args) {
+        class NewPaltar {
+            int sayi;
+            String material;
+            String dizayn;
+            String forma;
+            String reng;
+        }
+//        NewPaltar paltar1 = new NewPaltar();
+//        paltar1 .sayi =2;
+//        paltar1.material = "Ipek";
+//        paltar1.forma = "Kostyum";
+//        paltar1.reng = "Yasil";
+//
+//        System.out.println(paltar1.sayi);
+//        System.out.println(paltar1.dizayn);
+//        System.out.println(paltar1.material);
+//        System.out.println(paltar1.forma);
+//
+
+        NewPaltar paltar2 = new NewPaltar();
+        paltar2 .sayi =2;
+        paltar2.material = "Spatel";
+        paltar2.forma = "Platya";
+        paltar2.reng = "Ag";
+        paltar2.dizayn = "casual";
+
+        System.out.println(paltar2.sayi);
+        System.out.println(paltar2.dizayn);
+        System.out.println(paltar2.material);
+        System.out.println(paltar2.forma);
+        System.out.println(paltar2.dizayn);
+
+    }
+}
 
 
